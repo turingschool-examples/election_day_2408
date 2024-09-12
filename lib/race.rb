3a562177e9f797fd72e34b0ebc31a5f00c45529d
Seth Verrill <verrill.seth@gmail.com>
2024-09-12 16:05:06 -0600

    feat: Add code for Race class and method to register candidates

--- lib/race.rb
+++ lib/race.rb
@@ -1,2 +1,13 @@
 class Race
+  attr_reader :office,
+              :candidates
+              
+  def initialize(office)
+    @office = office
+    @candidates = []
+  end
+
+  def register_candidate!(candidate)    
+    @candidates << candidate    
+  end
 end
