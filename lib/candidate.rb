55160febe83cccb118e9e7d03def0659e7f3ec30
Seth Verrill <verrill.seth@gmail.com>
2024-09-12 16:03:25 -0600

    feat: Add code for a Candidate class with a method to add votes

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,2 +1,14 @@
 class Candidate
+  attr_reader :name,
+              :party,
+              :votes
+  def initialize(attributes)
+    @name = attributes[:name]
+    @party = attributes[:party]
+    @votes = 0
+  end
+
+  def vote_for!
+    @votes += 1
+  end
 end
