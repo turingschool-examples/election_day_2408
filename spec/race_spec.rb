8478cd3ab431f670ade3ae88f2b7030039d08425
Alora Riley <alora.riley@gmail.com>
2024-09-12 15:21:55 -0600

    test: Pass all tests in iteration 2

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -3,8 +3,6 @@ require './spec/spec_helper'
 RSpec.describe "Race" do
   before(:each) do
     @race = Race.new("Texas Governor")
-    @candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
-    @candidate2 = @race.register_candidate!({name: "Roberto R", party: :republican})
   end 
 
   describe "#exists" do
@@ -19,20 +17,26 @@ RSpec.describe "Race" do
     end
   end
 
-    xit "candidates start off an empty array" do
+    it "candidates start off an empty array" do
       expect(@race.candidates).to eq([])
     end
 
-    xit 'candidates are an instance from the Candidate class' do
+  describe '#Add candidates to the array' do
+    before(:each) do
+    @candidate1 = @race.register_candidate!({name: "Diana D", party: :democrat})
+    @candidate2 = @race.register_candidate!({name: "Roberto R", party: :republican})
+    end
+
+    it 'candidates are an instance from the Candidate class' do
       expect(@candidate1.class).to eq(Candidate)
     end
 
-    xit 'candidate has a name' do
+    it 'candidate has a name' do
       expect(@candidate1.name).to eq("Diana D")
     end
 
     it 'can update the candidates array' do
       expect(@race.candidates).to eq([@candidate1, @candidate2])
     end
-
+  end
 end
