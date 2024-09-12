137693415ffc2446c949cf35c604524a37dbbb0d
Alora Riley <alora.riley@gmail.com>
2024-09-12 15:59:03 -0600

    feat: Add initialize and add_races

--- lib/election.rb
+++ lib/election.rb
@@ -1,2 +1,18 @@
 class Election
+  attr_reader :year, :races
+
+  def initialize(year)
+    @year = year
+    @races = []
+  end
+
+  def add_race(office)
+    @races << Race.new(office)
+    return @races
+  end
+
+  # def vote_counts
+  #   race.candidates.each do |candidate|
+  #     candidate.each do |name, party|
+
 end
