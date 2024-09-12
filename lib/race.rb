d14a5c4170d0f2564d4b538535e355f4a452b2af
Alora Riley <alora.riley@gmail.com>
2024-09-12 07:51:43 -0600

    feat: Initialize class with Candidate module

--- lib/race.rb
+++ lib/race.rb
@@ -1,2 +1,15 @@
+require './lib/candidate'
+
 class Race
+  include Candidate
+  attr_reader :office, :candidates
+
+  def initialize(office)
+    @office = office
+    @candidates = []
+  end
+
+  def register_candidate!(candidates)
+    
+  end
 end
