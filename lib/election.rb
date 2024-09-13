59170d1e8aaa98029510e8912f0f15f653663b12
Alora Riley <alora.riley@gmail.com>
2024-09-13 09:03:26 -0600

    feat: Attempt last two methods from iteration 3

--- lib/election.rb
+++ lib/election.rb
@@ -1,9 +1,10 @@
 class Election
-  attr_reader :year, :races
+  attr_reader :year, :races, :candidates
 
   def initialize(year)
     @year = year
     @races = []
+    @candidates = []
   end
 
   def add_race(office)
@@ -11,8 +12,16 @@ class Election
     return @races
   end
 
-  # def vote_counts
-  #   race.candidates.each do |candidate|
-  #     candidate.each do |name, party|
+  def add_candidate_to_election(candidate_info)
+    candidate = Candidate.new(candidate_info)
+    @candidates << candidate
+  end
+
+  def candiate_name(candidates)
+    candidates.map do |candiate|
+      candidate.name
+    end
+  end
+
 
 end
